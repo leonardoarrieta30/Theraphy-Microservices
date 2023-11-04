47a41ede3a4b970fbafb89657fd76f8ddd9b8e4c
MaxSabinoA <103917940+MaxSabinoA@users.noreply.github.com>
2023-11-04 13:16:39 -0500

    feat: Update Diagnosis.java

--- healthrecordandexpertise/src/main/java/com/digitalholics/healthrecordandexpertise/domain/model/entity/Diagnosis.java
+++ healthrecordandexpertise/src/main/java/com/digitalholics/healthrecordandexpertise/domain/model/entity/Diagnosis.java
@@ -2,9 +2,7 @@ package com.digitalholics.healthrecordandexpertise.domain.model.entity;
 
 
 
-//import com.digitalholics.iotheraphy.Profile.domain.model.entity.Patient;
-//import com.digitalholics.iotheraphy.Profile.domain.model.entity.Physiotherapist;
-//import com.fasterxml.jackson.annotation.JsonFormat;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
